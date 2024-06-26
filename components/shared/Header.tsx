e6220034153c9636a2a3d67dfc99b3f70296b213
Shalabh Agarwal <agarwalshalabh98@gmail.com>
2024-06-26 12:00:36 +0530

    added jest files

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,48 +1,44 @@
-  import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-  import Image from "next/image";
-  import Link from "next/link";
-  import React from "react";
-  import { Button } from "../ui/button";
-  import MobileNav from "./MobileNav";
-  import Navitems from './Navitems';
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import MobileNav from "./MobileNav";
+import Navitems from "./Navitems";
 
-  const Header = () => {
-    return (
-      <header className="w-full border-b">
-        <div className="wrapper flex items-center justify-between">
-          <Link href="/" className="w-36">
-            <Image
-              src="/assets/images/logo.svg"
-              width={128}
-              height={38}
-              alt="Evently logo"
-            ></Image>
-          </Link>
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          ></Image>
+        </Link>
 
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <Navitems></Navitems>
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3 ">
           <SignedIn>
-            <nav className="md:flex-between hidden w-full max-w-xs">
-              <Navitems></Navitems>
-            </nav>
-
+            <UserButton afterSignOutUrl="/"></UserButton>
+            <MobileNav />
           </SignedIn>
-
-          <div className="flex w-32 justify-end gap-3 ">
-            <SignedIn>
-              <UserButton afterSignOutUrl="/"></UserButton>
-              <MobileNav />
-            </SignedIn>
-            <SignedOut>
-              <Button asChild className="rounded-full" size="lg">
-                  <Link href="/sign-in">
-                    Login
-                  </Link>
-
-              </Button>
-            </SignedOut>
-          </div>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </div>
-      </header>
-    );
-  };
+      </div>
+    </header>
+  );
+};
 
-  export default Header;
+export default Header;
